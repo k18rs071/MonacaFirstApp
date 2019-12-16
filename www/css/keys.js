84bb2ed751fe974832c245f56cef0c2d02d9f0f7
k18rs071 <k18rs071@st.kyusan-u.ac.jp>
2019-12-16 13:22:28 +0900

    keys.jsファイルの作成

---
+++ www/css/keys.js
@@ -0,0 +1,6 @@
+// This is a JavaScript file
+var appKey = "99d124d09746a55c83f477557e15b935ffee47bf92d6701cb266972193f6a3f3";
+
+var clientKey = "f9877e09844a6835f0aefa9739199140c8e3c1a0bba9e482b11d228a53732ac3";
+
+
